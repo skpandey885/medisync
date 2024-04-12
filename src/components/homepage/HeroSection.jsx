7b11823405c72bf1a2c26970cdfe597226018c94
Mehul <purimehul2003@gmail.com>
2024-04-13 00:17:11 +0530

    improved hero section

--- src/components/homepage/HeroSection.jsx
+++ src/components/homepage/HeroSection.jsx
@@ -1,49 +1,51 @@
 import React from "react";
 
-
 const Hero = () => {
-    return (
-        <div>
-            <>
-                <section className="px-2 py-32 bg-white dark:bg-main-dark-bg md:px-0">
-                    <div className="container items-center max-w-screen-xl px-8 mx-auto xl:px-5">
-                        <div className="flex flex-wrap items-center sm:-mx-3">
-                            <div className="w-full md:w-1/2 md:px-3">
-                                <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                                    <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                                        <span className="block xl:inline">DeSIDB</span>
-                                        <span className="block text-5xl font-medium text-blue-500">
-                                            Decentralized Student Information Database
-                                        </span>
-                                    </h1>
-                                    <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                                   A decentralised database to store &amp; access the data of students and colleges, to allow educational organisations to track the information.
-                                    </p>
-                                    <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                                        <a
-                                            href="/register/student"
-                                            className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-blue-500 rounded-md sm:mb-0 hover:bg-blue-700 sm:w-auto"
-                                        >
-                                            Get Started
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="w-full md:w-1/2">
-                                <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                                    <img
-                                        src="https://miro.medium.com/max/580/1*5AKv5i_omy24uvWcz64NpA.png"
-                                        className="object-center object-cover h-full w-full" alt="..."
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </>
+  return (
+    <div>
+      <>
+        <section className="px-2 py-32 bg-white dark:bg-main-dark-bg md:px-0">
+          <div className="container items-center max-w-screen-xl px-8 mx-auto xl:px-5">
+            <div className="flex flex-wrap items-center sm:-mx-3">
+              <div className="w-full md:w-1/2 md:px-3">
+                <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                  <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+                    <span className="block xl:inline">GovHSP</span>
+                    <span className="block text-5xl font-medium text-blue-500">
+                      Goverment Health Services Portal
+                    </span>
+                  </h1>
+                  <p className="mx-auto text-center text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
+                    Your digital gateway to comprehensive healthcare
+                    information, services, and resources, ensuring access to
+                    quality care for all citizens.
+                  </p>
 
-        </div>
-    )
-}
+                  <div className="relative flex flex-col sm:flex-row sm:space-x-4">
+                    <a
+                      href="/register/student"
+                      className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-blue-500 rounded-md sm:mb-0 hover:bg-blue-700 sm:w-auto"
+                    >
+                      Get Started
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2">
+                <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                  <img
+                    src="https://img.freepik.com/free-vector/medics-working-charts_1262-19836.jpg?t=st=1712944955~exp=1712948555~hmac=84da9801e456a3a1566a986d65798e915aa96b12677933b1698a974c3a4433d1&w=1060"
+                    className="object-center object-cover h-full w-full"
+                    alt="medical image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/homepage/Info.jsx
+++ src/components/homepage/Info.jsx
@@ -1,58 +1,115 @@
-import React from 'react'
-import { GiStrong } from 'react-icons/gi'
-import { FaExpandArrowsAlt } from 'react-icons/fa'
-import { MdLock } from 'react-icons/md'
+import React from "react";
+import {
+  GiLightBulb,
+  GiMagnifyingGlass,
+  GiMedicinePills,
+  GiMedicines,
+  GiStrong,
+} from "react-icons/gi";
+import { FaExpandArrowsAlt } from "react-icons/fa";
+import {
+  MdIntegrationInstructions,
+  MdLightbulb,
+  MdLock,
+  MdMedicalServices,
+  MdSchedule,
+} from "react-icons/md";
 
 const Info = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <section className="py-8 bg-white dark:bg-main-dark-bg">
+        <div className="container max-w-6xl mx-auto">
+          <h2 className="text-4xl font-bold tracking-tight text-center">
+            Our Features
+          </h2>
+          <p className="mt-2 text-lg text-center text-gray-600">
+            Unlock a world of streamlined healthcare access and transparency
+            through our cutting-edge features
+          </p>
+          <div className="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
+            <div className="relative flex flex-col items-center justify-center col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <GiMedicines size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Medicine Availability Tracking
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Ensures up-to-date information on medicine availability through
+                real-time tracking.
+              </p>
+            </div>
 
-            <section className="py-8 bg-white dark:bg-main-dark-bg">
-                <div className="container max-w-6xl mx-auto">
-                    <h2 className="text-4xl font-bold tracking-tight text-center">
-                        Our Features
-                    </h2>
-                    <p className="mt-2 text-lg text-center text-gray-600">
-                        We have integrated industry standard smart contracts to make the data safe and unmodifiable
-                    </p>
-                    <div className="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
-                        <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-                            <div className="p-3 text-white bg-blue-500 rounded-full">
-                                <GiStrong size={40} />
-                            </div>
-                            <h4 className="text-xl font-medium text-gray-700">Robust</h4>
-                            <p className="text-base text-center text-gray-500">
-                                Our App makes use of modern technologies and superior front end libraries.
-                            </p>
-                        </div>
+            <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <MdSchedule size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Doctor Scheduling and Appointment Booking
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Facilitates seamless scheduling and booking of doctor
+                appointments for efficient healthcare management.
+              </p>
+            </div>
 
-                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
-                            <div className="p-3 text-white bg-blue-500 rounded-full">
+            <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <GiMagnifyingGlass size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Transparent Billing System
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Implements a transparent billing system to provide clarity and
+                accountability in healthcare expenses
+              </p>
+            </div>
 
-                                <FaExpandArrowsAlt size={40} />
-                            </div>
-                            <h4 className="text-xl font-medium text-gray-700">Scalable</h4>
-                            <p className="text-base text-center text-gray-500">
-                                With our industry standard integrations, scalability will be as easy as a pie.
-                            </p>
-                        </div>
+            <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <MdMedicalServices size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Comprehensive Service Catalogue
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Offers a comprehensive catalogue of healthcare services to meet
+                diverse needs effectively.
+              </p>
+            </div>
 
+            <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <MdLightbulb size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Tailored Healthcare Awareness Section
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Provides a personalized healthcare awareness section to educate
+                and empower users with relevant information.
+              </p>
+            </div>
 
-                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
-                            <div className="p-3 text-white bg-blue-500 rounded-full">
-                                <MdLock size={40} />
-                            </div>
-                            <h4 className="text-xl font-medium text-gray-700">Secure</h4>
-                            <p className="text-base text-center text-gray-500">
-                                Our Smart Contract Ensures Tamper-proof data, and prevents data loss
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+            <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+              <div className="p-3 text-white bg-blue-500 rounded-full">
+                <MdIntegrationInstructions size={40} />
+              </div>
+              <h4 className="text-center text-xl font-medium text-gray-700">
+                Telemedicine Integration
+              </h4>
+              <p className="text-base text-center text-gray-500">
+                Integrates telemedicine services for convenient access to
+                healthcare professionals remotely.
+              </p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
