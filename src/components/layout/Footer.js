6f551ecfe4b41d56d4f0dc2549d1e1e49cb30c98
Mehul <purimehul2003@gmail.com>
2024-04-13 16:27:59 +0530

    healthcare inititatives

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { MdFacebook } from 'react-icons/md'
-import { BsTwitter } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
+import React from "react";
+import { MdFacebook } from "react-icons/md";
+import { BsTwitter } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div>
-    
-    <section className="bg-white dark:bg-main-dark-bg w-full  bottom-0 border-t">
+      <section className="bg-white dark:bg-main-dark-bg w-full  bottom-0 border-t">
         <div className="max-w-screen-xl px-4 py-5 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
@@ -36,12 +35,10 @@ const Footer = () => {
                 Terms
               </a>
             </div>
-
-            
           </nav>
 
           <div className="flex justify-center mt-8 space-x-6">
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            {/* <a href="#" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">Facebook</span>
               <MdFacebook size={30} />
             </a>
@@ -49,20 +46,19 @@ const Footer = () => {
             <a href="#" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">Twitter</span>
               <BsTwitter size={30} />
-            </a>
+            </a> */}
             <a href="#" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">GitHub</span>
               <BsGithub size={30} />
             </a>
-
           </div>
           <p className="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2022 TeamDEV
+            © 2024 IE14
           </p>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/others/HealthcareInitiatives.js
+++ src/pages/others/HealthcareInitiatives.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import {BsArrowRight} from 'react-icons/bs'
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 const HealthcareInitiatives = () => {
   return (
     <div>
-      
-        <section className="h-auto">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 text-center">
-            <p className="text-xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">
-              Get Information on various Healthcare Initiatives offered by the government
-            </p>
-            <p className="max-w-3xl mt-5 mx-auto text-xl text-gray-500">
-              Grow yourself and make your research easy with these government offered fellowships
-            </p>
-          </div>
-        </section>
-     
-
+      <section className="h-auto">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 text-center">
+          <p className="text-xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">
+            Get Information on various Healthcare Initiatives offered by the
+            government
+          </p>
+          <p className="max-w-3xl mt-5 mx-auto text-xl text-gray-500">
+            Discover how government healthcare initiatives are improving rural
+            wellbeing
+          </p>
+        </div>
+      </section>
 
       <section className="text-gray-600 body-font">
         <div className="container px-20 py-12 mx-auto">
           <div className="flex flex-wrap -m-4">
-
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-blue-500 mb-3">
-                  AICTE POST GRADUATE (PG) SCHOLARSHIP
+                  Aam Aadmi Bima Yojana
                 </h1>
                 <p className="leading-relaxed mb-3">
-                  P G Scholarship is awarded to full time GATE/GPAT qualified students admitted to M.E./ M.Tech /M. Arch and M.Pharma courses in AICTE approved Institutions/universities @Rs. 12,400/- PM/student for maximum 24 months
+                  The Aam Aadmi Bima Yojana (AABY) provides Rs. 30,000 annual
+                  insurance to laborers like weavers, fishermen, and carpenters.
+                  Eligibility requires being the household head or earning below
+                  the BPL threshold. Central Government Health Scheme benefits
+                  central government employees, pensioners, Delhi police, and
+                  ...
                 </p>
-                <a href='https://www.aicte-india.org/schemes/students-development-schemes/PG-Scholarship-Scheme' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://awbi.in/top-10-government-health-schemes-for-indian-citizens/#:~:text=Rajiv%20Aarogyasri%20Telangana-,Aam%20Aadmi%20Bima%20Yojana,-The%20Aam%20Aadmi"
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
@@ -38,13 +45,22 @@ const HealthcareInitiatives = () => {
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-blue-500 mb-3">
-                AICTE DOCTORAL FELLOWSHIP (ADF)
+                  Rashtriya Swasthya Bima Yojana
                 </h1>
                 <p className="leading-relaxed mb-3">
-                This scheme is implemented across 39 Universities in India for providing fellowship to the full time meritorious research scholars @ Rs 31,000 for first two years and Rs 35,000 thereafter
+                  One of the many government health programs in India introduced
+                  by the Ministry of Labor and Employment is the Rashtriya
+                  Swasthya Bima Yojana. This program provides medical coverage
+                  for laborers and other individuals working in unorganized
+                  sectors. As well as emergency health coverage for their
+                  families.
                 </p>
-                <a href='https://www.aicte-india.org/schemes/students-development-schemes' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://awbi.in/top-10-government-health-schemes-for-indian-citizens/#:~:text=of%20the%20household.-,Rashtriya%20Swasthya%20Bima%20Yojana,-One%20of%20the"
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
@@ -52,13 +68,21 @@ const HealthcareInitiatives = () => {
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-base	 font-medium text-blue-500 mb-3">
-                AICTE PRAGATI SCHOLARSHIP FOR MERITORIOUS GIRL STUDENTS
+                  Mahatma Jyotiba Phule Jan Arogya Yojana
                 </h1>
                 <p className="leading-relaxed mb-3">
-                Pragati Scholarship is awarded to meritorious Girl students taking admission in AICTE approved Technical institutions and courses at Degree/Diploma @ Rs. 50000 per annum for every year of study .
+                  The Maharashtra government's Mahatma Jyotiba Phule Jan Arogya
+                  Yojana provides healthcare coverage to economically
+                  disadvantaged segments. Policyholders can claim medical
+                  expenses up to ₹1.5 lakh for illnesses from the policy's
+                  issuance day, applicable to select distric
                 </p>
-                <a href='https://www.aicte-india.org/schemes/students-development-schemes/Pragati/General-Instructions' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://awbi.in/top-10-government-health-schemes-for-indian-citizens/#:~:text=or%20paper%20records.-,Mahatma%20Jyotiba%20Phule%20Jan%20Arogya%20Yojana,-The%20Mahatma%20Jyotiba"
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
@@ -66,13 +90,23 @@ const HealthcareInitiatives = () => {
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-blue-500 mb-3">
-                AICTE SAKSHAM SCHOLARSHIP FOR SPECIALLY ABLED STUDENTS
+                  Rajiv Aarogyasri Telangana
                 </h1>
                 <p className="leading-relaxed mb-3">
-                Saksham Scholarship is awarded to specially abled students taking admission in AICTE approved Technical institution and courses at Degree/Diploma. This scheme provides financial support @ Rs 50,000 per annum for every year of study to all eligible specially abled students.
+                  The Rajiv Aarogyasri Health Insurance Scheme was introduced by
+                  the Andhra Pradesh government as a low-income family plan.
+                  Beneficiaries of the health plan receive a host of benefits,
+                  including follow-up facilities, cashless medical services,
+                  coverage for pre-existing diseases, and outpatient care. The
+                  state is implementing the Rajiv Aarogyasri Scheme, a
+                  distinctive community health insurance program
                 </p>
-                <a href='https://www.aicte-india.org/schemes/students-development-schemes/Saksham/General-Instructions' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://awbi.in/top-10-government-health-schemes-for-indian-citizens/#:~:text=renew%20this%20premium.-,Rajiv%20Aarogyasri%20Telangana,-The%20Rajiv%20Aarogyasri"
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
@@ -80,13 +114,24 @@ const HealthcareInitiatives = () => {
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-blue-500 mb-3">
-                AICTE – SWANATH SCHOLARSHIP SCHEME
+                  National Rural Health Mission (NRHM)
                 </h1>
                 <p className="leading-relaxed mb-3">
-                AICTE has approved 'AICTE – SWANATH SCHOLARSHIP SCHEME' for children from any one of the following categories: 1. Orphan 2. Either or both parents died due to Covid-19 3. Wards of Armed Forces and Central Paramilitary Forces martyred in action (Shaheed) to provide them financial support of Rs. 50,000/- per annum for every year of study.
+                  Realizing the need to adopt an integrated systems approach,
+                  the government launched the NHRM to address the needs of the
+                  large Indian rural population. The long-term goal of the NHRM
+                  is to assure a fully functional and decentralized health
+                  delivery system. It also aims to provide intersectional
+                  coordination among the determinants of health, such as
+                  education, nutrition, sanitation, water, social equality, and
+                  others.
                 </p>
-                <a href='https://fellowship.aicte-india.org/Scheme%20Guidelines' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://www.smsfoundation.org/primary-healthcare-initiatives-can-tackle-poverty-in-rural-india/#:~:text=National%20Rural%20Health%20Mission%20(NRHM)."
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
@@ -94,24 +139,32 @@ const HealthcareInitiatives = () => {
             <div className="p-2 lg:w-1/2">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-lg overflow-hidden text-center relative">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-blue-500 mb-3">
-                AICTE - (YUVAK)
+                  INTEGRATED CHILD DEVELOPMENT SERVICES (ICDS)
                 </h1>
                 <p className="leading-relaxed mb-3">
-                  P G Scholarship is awarded to full time GATE/GPAT qualified students admitted to M.E./ M.Tech /M. Arch and M.Pharma courses in AICTE approved Institutions/universities @Rs. 12,400/- PM/student for maximum 24 months
+                  The population of children up through age six years as per the
+                  2011 census was 158 million and growing. This human capital of
+                  the country needs to be nurtured, especially in rural areas,
+                  to make them productive for the times to come. As part of the
+                  government’s commitment to capacity building for this
+                  vulnerable section of the population, the ICDS Scheme was
+                  launched in 1975 with a focus on children (0–6 years),
+                  pregnant women, and lactating mothers.
                 </p>
-                <a href='https://www.aicte-india.org/schemes/students-development-schemes/PG-Scholarship-Scheme' className="text-blue-600 inline-flex items-center">
-                  <span className='px-3 font-bold	'>Learn More</span>  <BsArrowRight size={20} />
+                <a
+                  href="https://www.aicte-india.org/schemes/students-development-schemes/PG-Scholarship-Scheme"
+                  className="text-blue-600 inline-flex items-center"
+                >
+                  <span className="px-3 font-bold	">Learn More</span>{" "}
+                  <BsArrowRight size={20} />
                 </a>
               </div>
             </div>
-
-            
           </div>
         </div>
       </section>
-
     </div>
-  )
-}
+  );
+};
 
-export default HealthcareInitiatives
+export default HealthcareInitiatives;
