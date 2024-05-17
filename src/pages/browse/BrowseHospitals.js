b8c010a3e51209255aabdca61e9915018ba88d3e
Sachin Pandey <skpandey885@gmail.com>
2024-05-18 01:29:20 +0530

    Search Functionality Bug Fixed

--- src/pages/browse/BrowseHospitals.js
+++ src/pages/browse/BrowseHospitals.js
@@ -6,7 +6,16 @@ import HospitalCard from "../../components/HospitalCard";
 import Loader from "../../components/Loader";
 function BrowseHospitals() {
   const [hospitals, setHospitals] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredHospitals, setFilteredHospitals] = useState([]);
 
+  useEffect(() => {
+    const filteredData = hospitals.filter((hospital) =>
+      hospital.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredHospitals(filteredData);
+  }, [searchQuery, hospitals]);
+  
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "hospitals"), (snapshot) => {
       const hospitalsData = [];
@@ -20,17 +29,28 @@ function BrowseHospitals() {
   }, []);
 
   return (
-    <div>
-      <h2 className='flex items-center gap-2 text-2xl font-bold text-gray-700 m-[20px]'>
-      /hospitals</h2>
+    <div className="flex flex-col items-center">
+      <h2 className="my-4 text-2xl font-semibold">Hospitals</h2>
+      <div className="relative w-full max-w-md mb-8">
+        <input
+          type="text"
+          placeholder="Search hospitals"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-full px-4 py-2 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
+     
 
-      {hospitals.length ?
-      <div className="flex flex-wrap gap-8 py-8 ">
-        {hospitals.map((hospital) => (
-          <HospitalCard key={hospital.id} hospital={hospital} />
-        ))}
-      </div> : <Loader/>}
+    {hospitals.length ?
+    <div className="flex flex-wrap gap-8 py-8 ">
+     {hospitals.map((hospital) => (
+     <HospitalCard key={hospital.id} hospital={hospital} />
+       ))}
+   </div> : <Loader/>}
     </div>
+     
+  
   );
 }
 
--- src/pages/browse/BrowseServices.js
+++ src/pages/browse/BrowseServices.js
@@ -7,6 +7,17 @@ import Loader from "../../components/Loader";
 
 function BrowseHospitals() {
   const [services, setServices] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredServices, setFilteredServices] = useState([]);
+  
+  
+  useEffect(() => {
+    const filteredData = services.filter((service) =>
+      service.department?.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredServices(filteredData);
+  }, [searchQuery, services]);
+  
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "services"), (snapshot) => {
@@ -21,19 +32,30 @@ function BrowseHospitals() {
   }, []);
 
   return (
-    <div>
-      <h2 className='flex items-center gap-2 text-2xl font-bold text-gray-700 m-[20px]'>
-      /services</h2>
-
+    <div className="flex flex-col items-center">
+      <h2 className="my-4 text-2xl font-semibold">Services</h2>
+      <div className="relative w-full max-w-md mb-8">
+        <input
+          type="text"
+          placeholder="Search services"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-full px-4 py-2 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
       {services.length ?
       <div className="flex flex-wrap gap-8 py-8 ">
         {services.map((service) => (
           <ServiceCard key={service.id} service={service} />
         ))}
       </div>
-: <Loader/>}
+: <Loader/> }
+
     </div>
-  );
-}
+
+
+
+
+);}
 
 export default BrowseHospitals;
