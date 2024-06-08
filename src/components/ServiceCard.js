c5287bff417c311bdd2cd36ee391e60ee845b948
Mehul <purimehul2003@gmail.com>
2024-06-08 11:18:27 +0530

    removed verify student button

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -53,12 +53,12 @@ const ServiceCard = ({ service }) => {
               </span>
             </li>
           </ul>
-          <button
+          {/* <button
             type="button"
             className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
           >
             Verify Student
-          </button>
+          </button> */}
         </div>
       </Link>
     </div>
